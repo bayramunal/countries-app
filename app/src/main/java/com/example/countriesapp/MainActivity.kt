faca8c637ec237b1cbdca66be7952809621491f5
Bayram ÜNAL <hacibayram.unal@gmail.com>
2020-08-08 19:41:51 +0300

    **

--- app/src/main/java/com/example/countriesapp/MainActivity.kt
+++ app/src/main/java/com/example/countriesapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.countriesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btnToCountryFragmentClick (view: View?) {
+        val routingAction = FeedFragmentDirections.actionFeedFragmentToCountryFragment()
+        view?.findNavController()?.navigate(routingAction)
+    }
+
+    fun btnToFeedFragmentClick (view: View?) {
+        val routingAction = CountryFragmentDirections.actionCountryFragmentToFeedFragment()
+        view?.findNavController()?.navigate(routingAction)
+    }
+
 }
